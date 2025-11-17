056a424cd61863b33a44acadab0ae5bdeebfb170
Chenghao Yu <https://github.com/chenghaoyu4330>
2025-11-17 15:07:55 +0800

    Finished lab04

--- lab04/ex2.s
+++ lab04/ex2.s
@@ -30,9 +30,12 @@ main:
 # a1 contains the power to raise to
 # the return value should be the result of a0^a1
 #     where ^ is the exponent operator, not XOR
+# 原代码出现死循环，原因是没有在调用下一层函数前保存ra，导致无法返回到main函数，一直在ex2中的49行-63行循环
 ex2:
     # Note: Add code BELOW without altering existing lines.
+    addi sp, sp, -8  # 开辟栈空间
     sw s0 0(sp)
+    sw ra, 4(sp)  # 存储返回地址
 
     # return 1 if a1 == 0
     beq a1 x0 ex2_zero_case
@@ -45,6 +48,7 @@ ex2:
 
     mul a0 a0 s0  # multiply ex2(a0, a1-1) by s0
                   # (which contains the value of a0)
+                  # 从这里开始死循环
 
     j ex2_end
 
@@ -53,5 +57,7 @@ ex2_zero_case:
 
 ex2_end:
     lw s0 0(sp)
+    lw ra, 4(sp)  # 复原返回地址
+    addi sp, sp, 8  # 回收栈空间
 
-    jr ra
+    jr ra  # 又跳回到49行
--- lab04/ex3.s
+++ lab04/ex3.s
@@ -40,7 +40,15 @@ ex3:
     mv t0 a0      # save a0 in t0
     addi a1 a1 -1 # decrement a1
 
+    addi sp, sp, -8
+    sw t0, 0(sp)
+    sw ra, 4(sp)  # 开辟栈空间并存储t0、ra，它们是调用者保存寄存器
+
     jal ra ex3    # call ex3(a0, a1-1)
+
+    lw t0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8  # 恢复t0、ra，回收栈空间
 
     mul a0 a0 t0  # multiply ex3(a0, a1-1) by t0
                   # (which contains the value of a0)
